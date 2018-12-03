3495aba4691d156fe0d076167ce76151b46fcda8
Jonathan Cameron <darsovit@gmail.com>
2018-12-03 10:11:55 -0700

    Advent of Code 2018, Day 3, Second
    The solution to the second half of the puzzle
    for Day 3 of Advent of Code 2018 is completed
    now. The change from first half of the puzzle
    was very minimal.

---
+++ Day3/Day3.5.py
@@ -0,0 +1,45 @@
+#!python
+'''
+Advent of Code 2018, Day 3, First problem
+Overlapping Elf plans
+'''
+
+def loadInput():
+    claims = dict()
+    with open('input.txt') as f:
+        for line in f:
+           (claim, at, pos, size) = line.split()
+           claim = int(claim[1:])
+           (left,top) = map(int, pos[:-1].split(','))
+           (width,height) = map(int, size.split('x'))
+           claims[claim] = {'left': left, 'top': top, 'width': width, 'height': height}
+    return claims
+
+def evaluatePlans( plans ):
+    fabric = []
+    for y in range(0,1000):
+        fabric = fabric + [bytearray(1000)]
+    overlapSquareInches = 0
+    for plan in plans.keys():
+        for y in range(plans[plan]['top'], plans[plan]['top']+plans[plan]['height']):
+            for x in range(plans[plan]['left'], plans[plan]['left']+plans[plan]['width']):
+                if (fabric[y][x] == 0):
+                    fabric[y][x] = 1
+                elif (fabric[y][x] == 1):
+                    fabric[y][x] = 2
+                    overlapSquareInches += 1
+    
+    for plan in plans.keys():
+        if planIsIntact(fabric, plans[plan]):
+            return plan
+    return 0
+
+def planIsIntact( fabric, plan ):
+    for y in range(plan['top'], plan['top']+plan['height']):
+        for x in range(plan['left'], plan['left']+plan['width']):
+            if fabric[y][x] == 2:
+                return False
+    return True
+
+elfPlans = loadInput()
+print( int(evaluatePlans(elfPlans)) )
