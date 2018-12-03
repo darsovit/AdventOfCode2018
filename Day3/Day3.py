e3dc30376fbc1154beebe548fed94c8683c45aee
Jonathan Cameron <darsovit@gmail.com>
2018-12-03 09:10:43 -0700

    Advent of Code 2018, Day 3, 1st Solution
    The solution to the first problem from Advent of Code
    2018, Day 3.

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -14,5 +14,21 @@ def loadInput():
            (width,height) = map(int, size.split('x'))
            claims[claim] = {'left': left, 'top': top, 'width': width, 'height': height}
     return claims
-    
+
+def evaluatePlans( plans ):
+    fabric = []
+    for y in range(0,1000):
+        fabric = fabric + [bytearray(1000)]
+    overlapSquareInches = 0
+    for plan in plans.keys():
+        for y in range(plans[plan]['top'], plans[plan]['top']+plans[plan]['height']):
+            for x in range(plans[plan]['left'], plans[plan]['left']+plans[plan]['width']):
+                if (fabric[y][x] == 0):
+                    fabric[y][x] = 1
+                elif (fabric[y][x] == 1):
+                    fabric[y][x] = 2
+                    overlapSquareInches += 1
+    return overlapSquareInches
+
 elfPlans = loadInput()
+print( int(evaluatePlans(elfPlans)) )
