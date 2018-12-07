cd400676050e9097740ff44f53695a0f169a2f0e
Jonathan Cameron <darsovit@gmail.com>
2018-12-07 15:55:48 -0700

    Advent of Code 2018, Day 7, Part 2
    Solution to the second half of the Day 7 problem.
    
    It's not too bad basing start on Day 7 part 1.
    
    https://adventofcode.com/2018/day/7

--- Day07/Day07.5.py
+++ Day07/Day07.5.py
@@ -19,6 +19,53 @@ def readInputGraph():
             if endNode not in graph['nodes']:
                 graph['nodes'].add(endNode)
     return graph
-    
-print( readInputGraph() )
-            
+
+def buildRunnableGraph( graph ):
+    graph['runnable'] = set()
+    for node in graph['nodes']:
+        graph['runnable'].add(node)
+    graph['dependencies'] = {}
+    graph['dependees']    = {}    
+    for edge in graph['edges']:
+        if edge[1] in graph['runnable']:
+            graph['runnable'].remove(edge[1])
+        if edge[1] not in graph['dependencies']:
+            graph['dependencies'][edge[1]] = set()
+        graph['dependencies'][edge[1]].add(edge[0])
+        if edge[0] not in graph['dependees']:
+            graph['dependees'][edge[0]] = set()
+        graph['dependees'][edge[0]].add(edge[1])
+    return graph
+
+def runGraph( numWorkers, graph ):
+    runOrder = []
+    ticks = 0
+    jobsProgressing = {}
+
+    while len( graph['runnable'] ) > 0 or len(jobsProgressing) > 0:
+        runnableList = list(graph['runnable'])
+        runnableList.sort()
+        while len(jobsProgressing) < numWorkers and len(runnableList) > 0:
+            nodeToRun = runnableList.pop(0)
+            jobsProgressing[nodeToRun] = ord(nodeToRun) - ord('A') + 61            
+            graph['runnable'].remove(nodeToRun)
+
+        ticks += 1
+        jobsToRemove = []
+        for job in jobsProgressing:
+            jobsProgressing[job] -= 1
+            if jobsProgressing[job] == 0:
+                jobsToRemove += [job]
+                if job in graph['dependees']:
+                    for nodeMaybeRunnable in graph['dependees'][job]:
+                        graph['dependencies'][nodeMaybeRunnable].remove(job)
+                        if len(graph['dependencies'][nodeMaybeRunnable]) == 0:
+                            graph['runnable'].add( nodeMaybeRunnable )
+        for job in jobsToRemove:
+            del jobsProgressing[job]
+    return ticks
+
+graph = readInputGraph()
+graph = buildRunnableGraph( graph )
+print( runGraph(5, graph) )
+      
