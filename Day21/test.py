a78cd7a12e4bfadd4a041469bc6671e468c7f3e7
Jonathan Cameron <darsovit@gmail.com>
2018-12-25 11:18:44 -0700

    Advent of Code 2018, Day 21 Updated
    Some adjustments / optimizations to the Day 21
    puzzle solution.
    
    https://adventofcode.com/2018/day/21

--- Day21/test.py
+++ Day21/test.py
@@ -4,31 +4,32 @@ D = 0x10000
 A = 0x1000000
 setOfDs = set()
 lastAnswer = None
+firstAnswer = None
 repeatCount = 0
 while not D == A:
     C = D | 0x10000
     D = 10552971
     while True:
-        E = C & 0xFF
-        D = D + E
+
+        D = D + ( C & 0xFF )
         D = D & 0xFFFFFF
         D = D * 65899
         D = D & 0xFFFFFF
         if ( 256 > C ):
             break
-        E = 0
-        while (E+1)*256 <= C:
-            E += 1
-        C = E
-    print( D )
+        C = int( C / 256 )
+    #print( D )
     if not lastAnswer:
         lastAnswer = D
+        firstAnswer = D
         setOfDs.add( D )
     elif D in setOfDs:
-        print( 'Repeated D:', D, ' -- last answer not repeated: ', lastAnswer )
+        #print( 'Repeated D:', D, ' -- last answer not repeated: ', lastAnswer )
         repeatCount += 1
         if repeatCount > 10:
             A = D
     else:
         setOfDs.add( D )
         lastAnswer = D
+print( 'First:', firstAnswer )
+print( 'Last non-repeated:', lastAnswer )
