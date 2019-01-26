2e2d022fb15e0afaecb977819f3474a95a05d9fb
Jonathan Cameron <darsovit@gmail.com>
2019-01-26 13:48:42 -0700

    Day 17 tests 7,8, and 9 added.
    Test 8 and 9 fail, but test 9 seems more
    'basic' than test 8 even though it may be
    a harder to follow test.

--- Day17/Day17.tdd.py
+++ Day17/Day17.tdd.py
@@ -36,26 +36,81 @@ def calcFlow(clayScanLines):
     return countFlowThroughRasterLines( scanLines )
 
 def test1():
+    # Empty set
     assert 0 == calcFlow([])
 
 def test2():
+    # (0)     x
+    # (1)   ##|
+    # (2)   ..|
     assert 1 == calcFlow(['y=1,x=498..499'])
 
 def test3():
+    # (0)    x
+    # (1)   #|
+    # (2)   #|
+    # (3)   .|
     assert 2 == calcFlow(['x=499,y=1..2'])
     
 def test4():
+    # (0)      x
+    # (1)     .|
+    # (2..99) .|
+    # (100)   #|
+    # (101)   #|
+    # (102)   .|
     assert 2 == calcFlow(['x=499,y=100..101'])
 
 def test5():
+    # (0)      x
+    # (1)    ##|    1st input
+    # (2)    ..|
+    # (3)    ..|
+    # (4)    ##|    2nd input
+    # (5)    ..|
     assert 4 == calcFlow(['y=1,x=498..499','y=4,x=498..499']), calcFlow(['y=1,x=498..499','y=4,x=498..499'])
 
 def test6():
+    # (0)      x
+    # (1)    ##|    2nd input
+    # (2)    ..|
+    # (3)    ..|
+    # (4)    ##|    1st input
+    # (5)    ..|
     assert 4 == calcFlow(['y=4,x=498..499','y=1,x=498..499']), calcFlow(['y=4,x=498..499','y=1,x=498..499'])
     
+def test7():
+    # (0)      x
+    # (1)    ##|
+    # (2)    ..|
+    # (3)    ..|#
+    # (4)    ..|#
+    # (5)    ..|#
+    assert 5 == calcFlow(['y=1,x=498..499','x=501,y=3..5'])
+
+def test8():
+    # (0)     x
+    # (1)    .|..
+    # (2)    ||##
+    # (3)    |##.
+    # (4)    |...
+    assert 3 == calcFlow(['y=2,x=501..502','y=3,x=500..501']), calcFlow(['y=2,x=501..502','y=3,x=500..501'])
+
+def test9():
+    # (0)    x
+    # (1)   .|.
+    # (2)   |||
+    # (3)   |#|
+    # (4)   |#|
+    # (5)   |.|
+    assert 4 == calcFlow(['x=500,y=3..4']), calcFlow(['x=500,y=3..4'])
+
 test1()
 test2()
 test3()
 test4()
 test5()
 test6()
+test7()
+#test8()
+test9()
